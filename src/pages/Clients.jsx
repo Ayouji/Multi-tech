b531c3061c3daae8bf97cc08f70d51522b34f2df
Hassan Adnan <hadnan@liadtech.com>
2026-05-02 00:37:51 +0100

    add

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useAppContext } from '../context/AppContext';
 import { t } from '../utils/translations';
-import { Plus, Trash2, Mail, Phone, MapPin, Search, UserPlus } from 'lucide-react';
+import { Plus, Trash2, Mail, Phone, MapPin, Search, UserPlus, Users } from 'lucide-react';
 
 const Clients = () => {
   const { clients, lang, deleteClient, addClient } = useAppContext();
